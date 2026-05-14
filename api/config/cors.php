6467a15b2fbb5d233fa2c59a20583390de2bb097
DyDev-1 <longdykry0410@gmail.com>
2026-05-14 23:14:51 +0700

    fix

--- api/config/cors.php
+++ api/config/cors.php
@@ -6,12 +6,13 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => array_values(array_unique(array_filter([
-        // env('FRONTEND_URL', 'http://localhost:3000'),
-        'https://clothes-ecommerce-docker.vercel.app',
-        // env('FRONTEND_URL_ALT', 'http://127.0.0.1:3000'),
+        env('FRONTEND_URL', 'https://clothes-ecommerce-docker.vercel.app'),
+        env('FRONTEND_URL_ALT', 'http://127.0.0.1:3000'),
     ]))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https:\/\/([a-z0-9-]+\.)*vercel\.app$#i',
+    ],
 
     'allowed_headers' => ['*'],
 
